9db9d66b349409be205788b8e6b4d72610f410a8
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-03-09 12:34:24 -0300

    Simple post method for create account

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello FinAPI"});
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { name, cpf } = req.body;
+
+  const id = uuidv4();
+
+  customers.push({
+    name,
+    cpf,
+    id,
+    statement: [],
+  });
+  
+  return res.status(201).send();
 })
 
 app.listen(3333);
