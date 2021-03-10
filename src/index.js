e2d023f0fa483f966d4ddfeec49d503cc76d529b
viniciussaturnino <viniciussaturnino07@hotmail.com>
2021-03-10 10:03:00 -0300

    Created post and get methods for a new customer and list statements

--- src/index.js
+++ src/index.js
@@ -5,21 +5,44 @@ const app = express();
 
 app.use(express.json());
 
+// Middleware
+function verifyIfExistsAccountCPF(req, res, next) {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf == cpf);
+  
+  if(!customer)
+    return res.status(400).json({ error: "Customer not found." });
+
+  req.customer = customer;
+  return next();
+}
+
 const customers = [];
 
 app.post("/account", (req, res) => {
   const { name, cpf } = req.body;
 
-  const id = uuidv4();
+  const customerAlrearyExists = customers.some((customer) => customer.cpf === cpf);
+
+  if(customerAlrearyExists)
+    return res.status(400).json({ error: "Customer already exists." });
 
   customers.push({
     name,
     cpf,
-    id,
+    id: uuidv4(),
     statement: [],
   });
   
   return res.status(201).send();
-})
+});
+
+//app.use(verifyIfExistsAccountCPF);
+
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  return res.json(customer.statement);
+});
 
 app.listen(3333);
